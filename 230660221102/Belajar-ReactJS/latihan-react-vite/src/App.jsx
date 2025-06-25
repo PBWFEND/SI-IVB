b21e2470631efff32408c3ba7db849e7efe7e206
JajangKomara <230660221102.student.unsap.ac.id>
2025-06-25 23:02:13 +0700

    Praktikum React Vite

---
+++ 230660221102/Belajar-ReactJS/latihan-react-vite/src/App.jsx
@@ -0,0 +1,16 @@
+// src/App.jsx
+import './App.css';
+
+function App() {
+  return (
+    <div>
+
+      
+
+      <h1 className="title">Hello! i Am Portgas D Ac</h1>
+      <p className="teks">Perkenalkan saya adalahahahahaha mahasiswa hihihi alamat saya di desa sukamaju kec. suka senang </p>
+    </div>
+  )
+}
+
+export default App
