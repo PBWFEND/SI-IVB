02f84137f64e26aebe469269557a849506aac1d8
230660221033 <230660221033@student.unsap.ac.id>
2025-06-27 23:38:42 +0700

    menambahkan modul 3a dan case study

---
+++ 230660221033/Praktikum-React/kartu-profil-app/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Card.css';
+
+function Card({ children}) {
+    return (
+        <div className="card-wrapper">
+            {children}
+        </div>
+    );
+}
+
+export default Card;
